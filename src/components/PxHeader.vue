7fc94a780315298d93fba3c615437b7e67c91d56
amoralestnle <alexander.morales@tnl.com.mx>
2020-05-11 17:05:36 -0500

    Cambio titulo

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -3,24 +3,17 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <px-icon class="mr-2" />
-          <router-link
-            :to="{ name: 'home' }"
-            class="font-semibold text-xl tracking-tight"
-            >Platzi Exchange</router-link
-          >
+          <PxIcon class="mr-2" />
+          <router-link :to="{ name: 'home' }" class="font-semibold text-xl tracking-tight">Exchange</router-link>
         </div>
-        <div
-          class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
-        >
+        <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow">
             <router-link
               v-for="l in links"
               :key="l.title"
               :to="l.to"
               class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-              >{{ l.title }}</router-link
-            >
+            >{{ l.title }}</router-link>
           </div>
         </div>
       </nav>
@@ -30,14 +23,15 @@
 
 <script>
 import PxIcon from "@/components/PxIcon";
+//import PxIconCustom from "@/components/PxIconCustom";
 export default {
   name: "PxHeader",
   components: { PxIcon },
   props: {
     links: {
       type: Array,
-      default: () => [],
-    },
-  },
+      default: () => []
+    }
+  }
 };
 </script>
